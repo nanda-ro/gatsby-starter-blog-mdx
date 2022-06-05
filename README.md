1536f3b2777328f63783ea6904d7f7fcb3198d0b
yuuki <you@nanda.ro>
2022-06-05 16:28:44 +0900

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # gatsby-starter-blog-mdx
+
+## Usage
+```
+gatsby new gatsby-starter-blog-mdx https://github.com/nanda-ro/gatsby-starter-blog-mdx
+```
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
     siteMetadata: {
-      title: "Blog title",
+      title: "Site Title",
       description: "Gatsby Starter with MDX supported",
     },
     plugins: [
